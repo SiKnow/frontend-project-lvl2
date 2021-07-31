0a9097ba567a20b0ce9ec1c6a8b582ebcc0ea05b
Sino Umarov <gf_nb@inbox.ru>
2021-07-31 22:49:45 +0300

    add format option on gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,5 @@ const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-
+  .option('-f, --format [type]', 'output format')
 program.parse();

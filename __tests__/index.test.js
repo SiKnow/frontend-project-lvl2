d88794ecc82c3f2c88e740e199d55799d33a1041
Sino Umarov <gf_nb@inbox.ru>
2021-08-23 00:43:43 +0300

    add __fixtures__ and make tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import { expect, test } from '@jest/globals';
+import path from 'path';
+import url from 'url';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('getData', () => {
+  const result = genDiff('file1.json', 'file2.json');
+  const answer = readFile('expect.txt').trim();
+  expect(result).toEqual(answer);
+});
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander/esm.mjs';
-import gendiff from '../index.js';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 

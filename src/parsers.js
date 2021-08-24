4e3451de893a073caa0b22ca1508897b8ffbb3a2
Sino Umarov <gf_nb@inbox.ru>
2021-08-24 19:41:06 +0300

    add parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import url from 'url';
+import yaml from 'js-yaml';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,8 +9,10 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const getData = (name) => {
+  const ext = path.extname(name);
   const data = readFile(name);
-  return JSON.parse(data);
+  const dataFile = (ext === '.yaml' || ext === '.yml') ? yaml.load(data) : JSON.parse(data);
+  return dataFile;
 };
 
 export default getData;

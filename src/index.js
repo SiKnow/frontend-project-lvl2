6f43987fe49e717ecb4473b84b7e941deb362e54
Sino Umarov <gf_nb@inbox.ru>
2021-09-15 00:19:12 +0300

    modified index,js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 /* eslint-disable no-unused-expressions */
-import _ from 'lodash';
 import getData from './parsers.js';
+import stylish from './formatters/stylish.js';
+import buildTree from './tree.js';
 
 const genDiff = (filePath1, filePath2) => {
   const file1 = getData(filePath1);
   const file2 = getData(filePath2);
 
-  const result = [];
-  const keys1 = _.keys(file1);
-  const keys2 = _.keys(file2);
-  const unionKeys = _.union(keys1, keys2).sort();
+  const tree = buildTree(file1, file2);
+  const formatter = stylish(tree);
 
-  unionKeys.forEach((key) => {
-    const inBoth = _.has(file1, key) && _.has(file2, key);
-    const inFirst = _.has(file1, key);
-    if (inBoth) {
-      (file1[key] === file2[key])
-        ? result.push(`  ${key}: ${file1[key]}`)
-        : result.push(`- ${key}: ${file1[key]}`, `+ ${key}: ${file2[key]}`);
-    } else {
-      (inFirst)
-        ? result.push(`- ${key}: ${file1[key]}`)
-        : result.push(`+ ${key}: ${file2[key]}`);
-    }
-  });
-  return `{\n ${result.join('\n ')}\n}`;
+  return formatter;
 };
 
 export default genDiff;

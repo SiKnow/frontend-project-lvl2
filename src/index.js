dab99c86bb8599bbff178ce32b5bb1b9a9590700
Sino Umarov <gf_nb@inbox.ru>
2021-09-19 00:20:56 +0300

    add stylish format

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-unused-expressions */
 import getData from './parsers.js';
-import stylish from './formatters/stylish.js';
 import buildTree from './tree.js';
+import formats from './formatters/index.js';
 
-const genDiff = (filePath1, filePath2) => {
+const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const file1 = getData(filePath1);
   const file2 = getData(filePath2);
 
   const tree = buildTree(file1, file2);
-  const formatter = stylish(tree);
 
-  return formatter;
+  return formats(tree, formatName);
 };
 
 export default genDiff;

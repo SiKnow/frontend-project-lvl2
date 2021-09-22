10db4abb97e320090f4a66f4894247de48c3efa6
Sino Umarov <sino.um@inbox.ru>
2021-09-22 19:33:49 +0300

    fix tests and bin file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,14 +9,32 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('getData', () => {
+test('.json format check', () => {
   const result = genDiff('file1.json', 'file2.json');
   const answer = readFile('expect.txt').trim();
   expect(result).toEqual(answer);
 });
 
-test('YAML', () => {
+test('.yaml format check', () => {
   const result = genDiff('file1.yaml', 'file2.yml');
+  const answer = readFile('expect.txt').trim();
+  expect(result).toEqual(answer);
+});
+
+test('plain (json files) format check', () => {
+  const result = genDiff('file1.json', 'file2.json', 'plain');
+  const answer = readFile('expected.txt').trim();
+  expect(result).toEqual(answer);
+});
+
+test('plain (yaml files) format check', () => {
+  const result = genDiff('file1.yaml', 'file2.yml', 'plain');
+  const answer = readFile('expected.txt').trim();
+  expect(result).toEqual(answer);
+});
+
+test('stylish format check', () => {
+  const result = genDiff('file1.yaml', 'file2.yml', 'stylish');
   const answer = readFile('expect.txt').trim();
   expect(result).toEqual(answer);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,9 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1>, <filepath2>')
-  .action((filePath1, filePath2) => console.log(gendiff(filePath1, filePath2)));
+  .action((filePath1, filePath2, formatName) => {
+    console.log(gendiff(filePath1, filePath2, formatName.format));
+  });
 program.parse();

0039a19bc60193b044d16f68fd33fb627b75fee5
Sino  Umarov <58088208+SiKnow@users.noreply.github.com>
2021-09-24 18:17:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-expressions */
 import getData from './parsers.js';
 import buildTree from './tree.js';
-import formats from './formatters/index.js';
+import format from './formatters/index.js';
 
 const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
-  const file1 = getData(filePath1);
-  const file2 = getData(filePath2);
+  const data1 = getData(filePath1);
+  const data2 = getData(filePath2);
 
-  const tree = buildTree(file1, file2);
+  const tree = buildTree(data1, data2);
 
-  return formats(tree, formatName);
+  return format(tree, formatName);
 };
 
 export default genDiff;

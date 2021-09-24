81bc7a4a50f34251a54596b384f8a81131b4a286
Sino  Umarov <58088208+SiKnow@users.noreply.github.com>
2021-09-24 18:29:34 +0300

    Update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -21,7 +21,7 @@ const plain = (tree) => {
       case 'change':
         return `Property '${name}' was updated. From ${toPlain(node.value1)} to ${toPlain(node.value2)}`;
       case 'nested':
-        return iter(node.child, [name]);
+        return iter(node.children, [name]);
       default:
         return [];
     }

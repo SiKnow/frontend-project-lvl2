5098c0b90a2c93c6a2a06692bf0c8139e3b44aa1
Sino Umarov <gf_nb@inbox.ru>
2021-09-30 19:35:55 +0300

    refactor: fix index.js and parsers.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-/* eslint-disable no-unused-expressions */
-import getData from './parsers.js';
+import path from 'path';
+import { readFileSync } from 'fs';
 import buildTree from './tree.js';
 import format from './formatters/index.js';
+import parse from './parsers.js';
 
-const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
+const getPath = (fileName) => path.resolve(process.cwd(), fileName);
+
+const getExtName = (fileName) => path.extname(fileName).substr(1);
+
+const getData = (filePath) => readFileSync(filePath, 'utf-8');
+
+const genDiff = (file1, file2, formatName = 'stylish') => {
+  const filePath1 = getPath(file1);
+  const filePath2 = getPath(file2);
+
+  const fileExt1 = getExtName(file1);
+  const fileExt2 = getExtName(file2);
+
   const data1 = getData(filePath1);
   const data2 = getData(filePath2);
 
-  const tree = buildTree(data1, data2);
+  const obj1 = parse(data1, fileExt1);
+  const obj2 = parse(data2, fileExt2);
+  const tree = buildTree(obj1, obj2);
 
   return format(tree, formatName);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,16 @@
-import path from 'path';
-import fs from 'fs';
-import url from 'url';
 import yaml from 'js-yaml';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
-const getData = (name) => {
-  const ext = path.extname(name);
-  const data = readFile(name);
-  const dataFile = (ext === '.yaml' || ext === '.yml') ? yaml.load(data) : JSON.parse(data);
-  return dataFile;
+const parse = (data, ext) => {
+  switch (ext) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+      return yaml.load(data);
+    case 'yml':
+      return yaml.load(data);
+    default:
+      throw new Error('Format Error!');
+  }
 };
 
-export default getData;
+export default parse;
